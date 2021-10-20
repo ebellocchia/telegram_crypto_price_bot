628cff3f745ee628216f1b76ad02f36d6ad376a4
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2021-10-20 14:15:12 +0200

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
+import os
 import setuptools
 import re
 
-VERSION_FILE = "telegram_crypto_price_bot/_version.py"
 
-with open("README.md", "r") as f:
-    long_description = f.read()
+# Load long description
+def load_long_description(desc_file):
+    return open(desc_file).read()
 
-def load_version():
-    version_line = open(VERSION_FILE).read().rstrip()
+
+# Load version
+def load_version(*path_parts):
+    version_file = os.path.join(*path_parts)
+    version_line = open(os.path.join(*path_parts)).read().rstrip()
     vre = re.compile(r'__version__: str = "([^"]+)"')
     matches = vre.findall(version_line)
 
     if matches and len(matches) > 0:
         return matches[0]
-    raise RuntimeError("Cannot find version string in %s" % VERSION_FILE)
 
-version = load_version()
+    raise RuntimeError(f"Cannot find version string in {version_file}")
+
+
+# Load requirements
+def load_requirements(req_file):
+    with open(req_file, "r") as fin:
+        return [line for line in map(str.strip, fin.read().splitlines())
+                if len(line) > 0 and not line.startswith("#")]
+
+
+# Load version
+version = load_version("telegram_crypto_price_bot", "_version.py")
 
 setuptools.setup(
     name="telegram_crypto_price_bot",
@@ -25,16 +39,16 @@ setuptools.setup(
     maintainer="Emanuele Bellocchia",
     maintainer_email="ebellocchia@gmail.com",
     description="Telegram bot for displaying cryptocurrencies price",
-    long_description=long_description,
+    long_description=load_long_description("README.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/ebellocchia/telegram_crypto_price_bot",
     download_url="https://github.com/ebellocchia/telegram_crypto_price_bot/archive/v%s.tar.gz" % version,
     license="MIT",
-    install_requires = ["pycoingecko", "matplotlib", "pyrogram", "tgcrypto", "apscheduler", "defusedxml"],
+    install_requires=load_requirements("requirements.txt"),
     packages=setuptools.find_packages(exclude=[]),
     package_data={"telegram_crypto_price_bot": ["lang/lang_en.xml"]},
     keywords="telegram, bot, telegram bot, crypto, crypto prices, cryptocurrency, cryptocurrency prices",
-    platforms = ["any"],
+    platforms=["any"],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
